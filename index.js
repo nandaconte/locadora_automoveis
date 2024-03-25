9df7be01f85deb3ee59b56f025d42a6f40a1bd83
nandaconte <fernanda.accsilva@senacsp.edu.br>
2024-03-24 21:32:19 -0300

    Verificar o select das listas

---
+++ index.js
@@ -0,0 +1,187 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const pool = require('./db/database');
+
+const app = express();
+
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.render('home')
+})
+
+//Clientes
+app.get('/clientes', function (req, res) {
+    res.render('clientes')
+});
+//Fim Clientes
+
+
+//Cadastrar Cliente
+app.get('/clientes/cadastrar', function (req, res) {
+    res.render('cadastrar-clientes')
+});
+app.post('/clientes/cadastrar', function (req, res) {
+    const cpf = req.body.cpf
+    const nome = req.body.nome;
+    const endereco = req.body.endereco;
+    const telefone = req.body.telefone;
+    const email = req.body.email;
+
+    const query = `INSERT INTO tbl_clientes (??,??,??,??,??) VALUES (?,?,?,?,?)`
+
+    const data = ['cpf', 'nome', 'endereco', 'telefone', 'email', cpf, nome, endereco, telefone, email]
+
+    pool.query(query, data, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/clientes')
+    })
+});
+//Fim Cadastrar Cliente
+
+//Lista de Clientes
+app.get('/clientes/lista', function (req, res) {
+    const query = `SELECT * FROM tbl_clientes`
+  
+    pool.query(query, function (err, data) {
+      if (err) {
+        console.log(err)
+      }
+      const clientes = data
+  
+      console.log(data)
+  
+      res.render('lista-clientes', { clientes })
+    })
+  });
+// Fim Lista de Clientes
+
+//Veiculos
+app.get('/veiculos', function (req, res) {
+    res.render('veiculos')
+});
+
+
+// Cadastrar Veículo
+app.get('/veiculos/cadastrar', function (req, res) {
+    res.render('cadastrar-veiculos')
+});
+app.post('/veiculos/cadastrar', function (req, res) {
+    const placa = req.body.placa
+    const marca = req.body.marca;
+    const modelo = req.body.modelo;
+    const ano = req.body.ano;
+    
+
+    const query = `INSERT INTO tbl_veiculos (??,??,??,??) VALUES (?,?,?,?)`
+
+    const data = ['placa', 'marca', 'modelo', 'ano', placa, marca, modelo, ano]
+
+    pool.query(query, data, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/veiculos')
+    })
+});
+//Fim Cadastro veiculo
+
+
+// Lista de Veiculos
+app.get('/veiculos/lista', function (req, res) {
+    const query = `SELECT * FROM tbl_veiculos`
+  
+    pool.query(query, function (err, data) {
+      if (err) {
+        console.log(err)
+      }
+      const veiculos = data
+  
+      console.log(data)
+  
+      res.render('lista-veiculos', { veiculos })
+    })
+  });
+  //Fim Lista de Veiculos
+
+
+
+// Reservas
+  app.get('/reservas', function (req, res) {
+    res.render('reservas')
+});
+//Fim Reservas
+
+
+
+//Cadastrar Reservas
+app.get('/reservas/cadastrar', function (req, res) {
+    res.render('cadastrar-reserva')
+});
+app.post('/reservas/cadastrar', function (req, res) {
+    const cpf = req.body.cpf
+    const placa = req.body.placa;
+    const dataInicio = req.body.dataInicio;
+    const dataFim = req.body.dataFim;
+    
+
+    const query = `INSERT INTO tbl_reservas (??,??,??,??) VALUES (?,?,?,?)`
+
+    const data = ['cpf', 'placa', 'dataInicio', 'dataFim', cpf, placa, dataInicio, dataFim]
+
+    pool.query(query, data, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/reservas')
+    })
+});
+//fim cadastrar reserva
+
+
+
+
+//Lista Reserva
+app.get('/reservas/lista', function (req, res) {
+    const query = `SELECT * FROM tbl_reservas`
+  
+    pool.query(query, function (err, data) {
+      if (err) {
+        console.log(err)
+      }
+      const reservas = data
+  
+      console.log(data)
+  
+      res.render('lista-reservas', {reservas})
+    })
+  });
+  //FIM LISTA RESERVA
+
+
+
+
+
+   
+
+
+
+// ===============================
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http:///localhost:${PORT}`)
+}); 
---
+++ script-locadora.sql
@@ -0,0 +1,23 @@
+create database locadora_automoveis
+use locadora_automoveis;
+create table  tbl_clientes(
+cpf char (12) primary key,
+nome char(100) not null,
+endereco varchar(255) not null,
+telefone char (15) not null,
+email char (100));
+select * from tbl_veiculos;
+create table tbl_veiculos(
+placa char (10) primary key,
+marca char (100) not null,
+modelo char (50) not null,
+ano char (4) not null);
+
+create table tbl_reservas(
+id_reserva int auto_increment primary key,
+cpf char (12),
+placa char(10),
+data_inicio datetime,
+date_fim datetime,
+foreign key (cpf) references tbl_clientes(cpf),
+foreign key (placa) references  tbl_veiculos(placa));
